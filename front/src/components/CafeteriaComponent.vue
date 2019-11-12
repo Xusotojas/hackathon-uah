ab57b71fd4905ca3d3ef288516589ac378b2481d
Francisco Jesús Jiménez Hidalgo <franciscojesus@jimenezhidalgo.es>
2019-11-12 18:28:21 +0100

    Added products

--- front/src/components/CafeteriaComponent.vue
+++ front/src/components/CafeteriaComponent.vue
@@ -1,4 +1,213 @@
-<template> <v-card></v-card></template>
+<template
+  ><v-container
+    ><v-row
+      ><v-card :loading="loading" class="mx-auto my-3" max-width="374">
+        <v-img height="250" src="../../public/cafe.jpg"></v-img>
+
+        <v-card-title>Café</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ml-4">4.5 (413)</div>
+          </v-row>
+
+          <div>
+            Café expresso, sin azúcar, como lo tomaban los espartanos libres.
+          </div>
+        </v-card-text>
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-title>Café Expresso</v-card-title>
+
+        <v-card-actions>
+          <v-btn color="deep-purple accent-4" text @click="blue">
+            Comprar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card :loading="loading" class="mx-auto my-3" max-width="374">
+        <v-img height="250" src="../../public/cafe.jpg"></v-img>
+
+        <v-card-title>Café</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ml-4">4.5 (413)</div>
+          </v-row>
+
+          <div>
+            Café expresso, sin azúcar, como lo tomaban los espartanos libres.
+          </div>
+        </v-card-text>
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-title>Café Expresso</v-card-title>
+
+        <v-card-actions>
+          <v-btn color="deep-purple accent-4" text @click="blue">
+            Comprar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card :loading="loading" class="mx-auto my-3" max-width="374">
+        <v-img height="250" src="../../public/cafe.jpg"></v-img>
+
+        <v-card-title>Café</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ml-4">4.5 (413)</div>
+          </v-row>
+
+          <div>
+            Café expresso, sin azúcar, como lo tomaban los espartanos libres.
+          </div>
+        </v-card-text>
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-title>Café Expresso</v-card-title>
+
+        <v-card-actions>
+          <v-btn color="deep-purple accent-4" text @click="blue">
+            Comprar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card :loading="loading" class="mx-auto my-3" max-width="374">
+        <v-img height="250" src="../../public/cafe.jpg"></v-img>
+
+        <v-card-title>Café</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ml-4">4.5 (413)</div>
+          </v-row>
+
+          <div>
+            Café expresso, sin azúcar, como lo tomaban los espartanos libres.
+          </div>
+        </v-card-text>
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-title>Café Expresso</v-card-title>
+
+        <v-card-actions>
+          <v-btn color="deep-purple accent-4" text @click="blue">
+            Comprar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card :loading="loading" class="mx-auto my-3" max-width="374">
+        <v-img height="250" src="../../public/cafe.jpg"></v-img>
+
+        <v-card-title>Café</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ml-4">4.5 (413)</div>
+          </v-row>
+
+          <div>
+            Café expresso, sin azúcar, como lo tomaban los espartanos libres.
+          </div>
+        </v-card-text>
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-title>Café Expresso</v-card-title>
+
+        <v-card-actions>
+          <v-btn color="deep-purple accent-4" text @click="blue">
+            Comprar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card :loading="loading" class="mx-auto my-3" max-width="374">
+        <v-img height="250" src="../../public/cafe.jpg"></v-img>
+
+        <v-card-title>Café</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ml-4">4.5 (413)</div>
+          </v-row>
+
+          <div>
+            Café expresso, sin azúcar, como lo tomaban los espartanos libres.
+          </div>
+        </v-card-text>
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-title>Café Expresso</v-card-title>
+
+        <v-card-actions>
+          <v-btn color="deep-purple accent-4" text @click="blue">
+            Comprar
+          </v-btn>
+        </v-card-actions>
+      </v-card></v-row
+    ></v-container
+  ></template
+>
 
 <script>
 export default {
